389a8ed5230b12af9c8d05c48719c0cf7e708cad
Winston Bandong <w.bandong@gmail.com>
2023-11-26 13:41:20 -0800

    forgot readme

---
+++ README.md
@@ -0,0 +1,11 @@
+---
+title: Gamedayspx Monitor
+emoji: 👀
+colorFrom: red
+colorTo: purple
+sdk: streamlit
+sdk_version: 1.28.2
+app_file: app.py
+pinned: false
+license: mit
+---
--- app.py
+++ app.py
@@ -55,198 +55,108 @@ daily_closes['FirstBar'] = dts
 levels = data_daily.loc[df1.index.date, ['H1','H2','L1','L2','Open']].drop_duplicates().reset_index()
 levels['FirstBar'] = dts
 
-# Create a candlestick trace with custom colors based on the CandleColor column
-candlestick_trace = go.Candlestick(
-    x=df1.index,
-    open=df1['Open'],
-    high=df1['High'],
-    low=df1['Low'],
-    close=df1['Close'],
-    increasing_fillcolor='#3399ff',
-    decreasing_fillcolor='#ff5f5f',
-    increasing_line_color='#3399ff',  # Color for decreasing candles
-    decreasing_line_color='#ff5f5f',  # Color for decreasing candles
-    name='30m'
-)
+# Plot
+import streamlit as st
+from streamlit_lightweight_charts import renderLightweightCharts
 
-df_up = df1.loc[df1['PredDir']=='Up']
-df_down = df1.loc[df1['PredDir']=='Down']
-df_neutral = df1.loc[df1['PredDir']=='Neutral']
+chartOptions = [{
+    "width":800,
+    "height":400,
+    "rightPriceScale": {
+        "scaleMargins": {
+            "top": 0.2,
+            "bottom": 0.25,
+        },
+        "borderVisible": False,
+    },
+    "layout": {
+        "textColor": 'white',
+        "background": {
+            "type": 'solid',
+            "color": 'black'
+        },
+    },
+    "grid": {
+        "vertLines": {
+            "color": "rgba(197, 203, 206, 0)"
+            },
+        "horzLines": {
+            "color": "rgba(197, 203, 206, 0)"
+        }
+    }
+},
+{
+    "width":800,
+    "height":125,
+    "layout": {
+        "textColor": 'white',
+        "background": {
+            "type": 'solid',
+            "color": 'black'
+        },
+    },
+    "grid": {
+            "vertLines": {
+                "color": "rgba(197, 203, 206, 0)"
+                },
+            "horzLines": {
+                "color": "rgba(197, 203, 206, 0)"
+            }
+        },
+},]
 
-scatter_up = go.Scatter(
-    x=df_up.index,
-    y=df_up['High'] * 1.001,
-    mode='markers',
-    marker=dict(size=8),
-    marker_color=df_up['Color'],
-    marker_symbol='triangle-up',
-    name='Up'
-)
+seriesCandlestickChart = [{
+    
+    "type": 'Candlestick',
+    "data": [
+        {"open": open, 
+         "high": high, 
+         "low": low, 
+         "close": close, 
+         "time": dt.timestamp()} for open, high, low, close, dt in zip(df1['Open'],df1['High'],df1['Low'],df1['Close'], df1.index)
+    ],
+    "options": {
+        "upColor": '#3399ff',
+        "downColor": '#ff5f5f',
+        "borderVisible": False,
+        "wickUpColor": '#3399ff',
+        "wickDownColor": '#ff5f5f',
+        "priceScaleVisible": True
+    },
+    "priceScale": {
+        "scaleMargins": {
+            "top": 0.7,
+            "bottom": 0,
+        }
+    }
+},
+{
+        "type": 'Line',
+        "data": [{"value": value, "time":dt.timestamp()} for value, dt in zip(levels['H1'], levels['FirstBar'])],
+        "options": {
+            "color": 'blue',
+            "lineWidth": 1
+        }
+    }]
 
-scatter_down = go.Scatter(
-    x=df_down.index,
-    y=df_down['Low'] * 0.999,
-    mode='markers',
-    marker=dict(size=8),
-    marker_color=df_down['Color'],
-    marker_symbol='triangle-down',
-    name='Down'
-)
+seriesPredictions = [{
+    "type": 'Histogram',
+    "data": [
+        { "value": pred, "time": dt.timestamp(), "color":color  } for pred, dt, color in zip(df1['CalibPredicted'], df1.index, df1['Color'])
+    ],
+    "options": { "color": '#26a69a' }
+}]
 
-scatter_neut = go.Scatter(
-    x=df_neutral.index,
-    y=df_neutral[['Open','High','Low','Close']].mean(axis=1),
-    mode='markers',
-    marker=dict(size=7),
-    marker_color=df_neutral['Color'],
-    marker_symbol='diamond-open',
-    name='Neutral'
-)
-
-# Create a layout
-layout = go.Layout(
-    title=dict(text='OHLC Chart with Predictions', xanchor='center', yanchor='top', y=0.9,x=0.5),
-
-    xaxis=dict(title='Date'),
-    yaxis=dict(title='Price'),
-    template='plotly_dark',
-    xaxis_rangeslider_visible=False,
-    width=750,
-    height=500
-)
-
-# Create a figure
-fig = go.Figure(data=[candlestick_trace, scatter_up, scatter_neut, scatter_down], layout=layout)
-
-fig.update_xaxes(
-        rangebreaks=[
-            # NOTE: Below values are bound (not single values), ie. hide x to y
-            dict(bounds=["sat", "mon"]),  # hide weekends, eg. hide sat to before mon
-            dict(bounds=[16, 9.5], pattern="hour"),  # hide hours outside of 9.30am-4pm
-            # dict(values=["2019-12-25", "2020-12-24"])  # hide holidays (Christmas and New Year's, etc)
-        ]
-    )
-
-fig.update_layout(
-    shapes = [dict(
-        x0=d-timedelta(minutes=15), x1=d-timedelta(minutes=15), y0=0, y1=1, xref='x', yref='paper',
-        line_width=0.5, opacity=0.5, line_dash='dot') for d in df1.loc[df1['ModelNum']==0].index],
-    legend=dict(yanchor="top", y=1.05, xanchor="center", x=0.5, orientation='h'),
-    margin=dict(l=20, r=20, t=80, b=20)
-)
-
-
-# Define the y-positions for your horizontal lines
-pairs = [(start, level) for start, level in zip(daily_closes['FirstBar'], daily_closes['PrevClose'])]
-
-# Add horizontal lines to the figure
-for pair in pairs:
-    start = pair[0] 
-    end = start + BDay(1) - timedelta(minutes=15)
-    level = pair[1]
-    fig.add_shape(
-        type="line",
-        x0=start,
-        x1=end,
-        y0=level,
-        y1=level,
-        xref='x',
-        yref='y',
-        line=dict(
-            width=0.5,
-            dash="dot",
-        ),
-    )
-
-for start in levels['FirstBar']:
-    end = start + BDay(1)-timedelta(minutes=15)
-    vals = levels.loc[levels['FirstBar']==start, ['H1','H2','L1','L2','Open']].values[0]
-    H1 = vals[0]
-    H2 = vals[1]
-    L1 = vals[2]
-    L2 = vals[3]
-    Open = vals[4]
-    # Plot H1
-    fig.add_shape(
-        type="line",
-        x0=start,
-        x1=end,
-        y0=H1,
-        y1=H1,
-        xref='x',
-        yref='y',
-        line=dict(
-            width=0.5,
-            dash="solid",
-            color="#ff5f5f"
-            ),
-        )
-    # Plot H2
-    fig.add_shape(
-        type="line",
-        x0=start,
-        x1=end,
-        y0=H2,
-        y1=H2,
-        xref='x',
-        yref='y',
-        line=dict(
-            width=1,
-            dash="solid",
-            color="#ff5f5f"
-            ),
-        )
-    # Plot L1
-    fig.add_shape(
-        type="line",
-        x0=start,
-        x1=end,
-        y0=L1,
-        y1=L1,
-        xref='x',
-        yref='y',
-        line=dict(
-            width=0.5,
-            dash="solid",
-            color="#3399ff"
-            ),
-        )
-    # Plot L2
-    fig.add_shape(
-        type="line",
-        x0=start,
-        x1=end,
-        y0=L2,
-        y1=L2,
-        xref='x',
-        yref='y',
-        line=dict(
-            width=1,
-            dash="solid",
-            color="#3399ff"
-            ),
-        )
-    # Plot Open
-    fig.add_shape(
-        type="line",
-        x0=start,
-        x1=end,
-        y0=Open,
-        y1=Open,
-        xref='x',
-        yref='y',
-        line=dict(
-            width=1,
-            dash="solid",
-            color="#cccccc"
-            ),
-        )
-
-fig.for_each_xaxis(lambda x: x.update(showgrid=False))
-fig.for_each_yaxis(lambda x: x.update(showgrid=False))
-
-# Show the figure
-st.plotly_chart(fig, use_container_width=True)
+renderLightweightCharts([
+    {
+        "chart": chartOptions[0],
+        "series": seriesCandlestickChart
+    },
+    {
+        "chart": chartOptions[1],
+        "series": seriesPredictions
+    },
+], 'multipane')
 
 # Important levels
 df_levels = pd.DataFrame(levels[['H2','H1','Open','L1','L2']].iloc[-1]).round(2)
